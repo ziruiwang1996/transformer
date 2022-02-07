e889b577ef48d07fecc6c67c582d972d4828506e
macM1 <wangzr926@gmail.com>
2022-02-07 17:54:19 -0500

    finished based transformer model

--- embedding.py
+++ embedding.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def onehot_emb (sequnece_string, d_model=21):
+# d_model = 21
+def onehot_emb (sequnece_string, d_model):
     tokens = ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y","-"]
     order = np.arange(len(tokens))
     look_up = dict(zip(tokens, order))
@@ -12,17 +13,17 @@ def onehot_emb (sequnece_string, d_model=21):
         n+=1
     return emb
 
-def seg_emb (list_of_tuples, seq_length, d_model=21):
+def seg_emb (list_of_tuples, seq_len, d_model):
     seg_id = np.arange(len(list_of_tuples))
-    emb = np.zeros((seq_length, d_model))
+    emb = np.zeros((seq_len, d_model))
     n= 0
     for tuple in list_of_tuples:
         emb[tuple[0]:tuple[1],:]=seg_id[n]
         n+=1
     return emb
 
-def pos_emb(seq_length, d_model=21, min_freq=1e-4):
-    pos = np.arange(seq_length)
+def pos_emb(seq_len, d_model, min_freq=1e-4):
+    pos = np.arange(seq_len)
     freqs = min_freq**(2*(np.arange(d_model)//2)/d_model)
     emb = pos.reshape(-1,1)*freqs.reshape(1,-1)
     emb[:, ::2] = np.cos(emb[:, ::2])
--- masking.py
+++ masking.py
@@ -11,6 +11,19 @@ for seq in seqs:
 data = np.array(A)
 #print(data.shape)
 
+seqs = ['AEAKYAEENCNACCSICPLPNLTISQRIAFIYALYDDPSQSSELLSEAKKLNDSQAPK',
+        'AEAKYAEENCNACCSICSLPNLTISQRIAFIYALYDDPSQSSELLSEAKKLNDSQAPK',
+        'AEAKYAEENCNACCSICSLPNLTISQRIAFVYALYDDPSQSSELLSEAKKLNDSQAPK',
+        'AEAKYAEENCNACCSICSLSNLTISQRIAFIYALYDDPSQSSELLSEAKKLNDSQAPK',
+        'AEAKYAEENCNALSEIYYLPNLTSTQRCAFIKALCDDPSQSSELLSEAKKLNDSQAPK']
+
+seq_list=[]
+for seq in seqs:
+        emb = onehot_emb(seq) + seg_emb( [(0,20),(20,38),(38,58)], 58) + pos_emb(58)
+        seq_list.append(emb)
+x_data = torch.tensor(np.array(seq_list))
+print(x_data.size())
+
 
 def token_masking(matrix_after_onehot=data, d_model=21, seq_lenth=16, sample_size=2, perc=0.15):
     mask = np.random.random((sample_size, seq_lenth))
