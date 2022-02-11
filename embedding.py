2a71a9a158e59ab2d53605ac487707f108e8d590
macM1 <wangzr926@gmail.com>
2022-02-10 20:43:17 -0500

    change to a function

--- embedding.py
+++ embedding.py
@@ -1,31 +1,44 @@
 import numpy as np
+import torch
 
-# d_model = 21
-def onehot_emb (sequnece_string, d_model):
-    tokens = ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y","-"]
-    order = np.arange(len(tokens))
-    look_up = dict(zip(tokens, order))
-    #dimension: seq_len * d_model
-    emb = np.zeros((len(sequnece_string), d_model))
-    n =0
-    for aa in list(sequnece_string):
-        emb[n][look_up.get(aa)]=1
-        n+=1
-    return emb
+# out Tensor shape: sample_size x seq_len x d_model
+def embedding(seq_strs, d_model, segment_pos):
+    embs = []
+    for seq_str in seq_strs:
+        seq_len = len(seq_str)
+        # one hot embedding
+        tokens = ["A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y"]
+        order = np.arange(len(tokens))
+        look_up = dict(zip(tokens, order))
+        one_hot_emb = np.zeros((seq_len, d_model))  # dimension: seq_len * d_model
+        n = 0
+        for aa in list(seq_str):
+            one_hot_emb[n][look_up.get(aa)] = 1
+            n += 1
 
-def seg_emb (list_of_tuples, seq_len, d_model):
-    seg_id = np.arange(len(list_of_tuples))
-    emb = np.zeros((seq_len, d_model))
-    n= 0
-    for tuple in list_of_tuples:
-        emb[tuple[0]:tuple[1],:]=seg_id[n]
-        n+=1
-    return emb
+        # segment embedding
+        seg_id = np.arange(len(segment_pos))
+        seg_emb = np.zeros((seq_len, d_model))
+        n = 0
+        for key, value in segment_pos.items():
+            seg_emb[int(key):int(value), :] = seg_id[n]
+            n += 1
 
-def pos_emb(seq_len, d_model, min_freq=1e-4):
-    pos = np.arange(seq_len)
-    freqs = min_freq**(2*(np.arange(d_model)//2)/d_model)
-    emb = pos.reshape(-1,1)*freqs.reshape(1,-1)
-    emb[:, ::2] = np.cos(emb[:, ::2])
-    emb[:, 1::2] = np.sin(emb[:, 1::2])
-    return emb
+        # positional embedding
+        pos = np.arange(seq_len)
+        min_freq = 1e-4
+        freqs = min_freq ** (2 * (np.arange(d_model) // 2) / d_model)
+        pos_emb = pos.reshape(-1, 1) * freqs.reshape(1, -1)
+        pos_emb[:, ::2] = np.cos(pos_emb[:, ::2])
+        pos_emb[:, 1::2] = np.sin(pos_emb[:, 1::2])
+        embs.append(one_hot_emb+seg_emb+pos_emb)
+    embs = np.array(embs)
+    return torch.from_numpy(embs)
+
+
+'''
+input = ['AEAKYAEENCNALSEIYYLPNLTSTQRCAFIKALCDDPSQSSELLSEAKKLNDSQAPK', 
+         'AEAKYAEENCNACCSICSLPNLTISQRIAFIYALYDDPSQSSELLSEAKKLNDSQAPK',
+         'AEAKYAEENCNACCSICSLSNLTISQRIAFIYALYDDPSQSSELLSEAKKLNDSQAPK']
+a = embedding(input, 20, {0:20, 20:38, 38:58})
+print(a.shape)'''
