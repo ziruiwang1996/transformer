1d99220ae843964f7f9549d176cdca9dbfe552d4
macM1 <wangzr926@gmail.com>
2022-02-22 02:16:26 -0500

    ready for training

--- bert_pre_training.py
+++ bert_pre_training.py
@@ -10,9 +10,8 @@ input_seqs = []
 for row in file:
     input_seqs.append(row[1])
 
-input_seqs = input_seqs[1:20]
+input_seqs = input_seqs[1:61]
 #print(input_seqs)
-
 
 class Bert(nn.Module):
     def __init__(self, d_model=20, num_layer=6, heads=4, device='cpu', ff_expansion=4, dropout=0.1):
@@ -29,52 +28,50 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(pre_train.parameters(), lr=0.001, momentum=0.9)
 
 masked_spots, indices_all_seqs = token_masking(input_seqs)
+masked_seqs = masked_seq(indices_all_seqs)
+labels_all = get_label(input_seqs, masked_spots)
 
-input = masked_seq(indices_all_seqs)
-label = labels(input_seqs, masked_spots)
-print(len(input), len(label))
+# make a iterable dataset
+dataset = []
+n = 0
+for seq in masked_seqs:
+    dataset.append([seq, n])
+    n += 1
 
-trainloader = torch.utils.data.DataLoader(input, batch_size=2, shuffle=True)
+batch_size = 6
+trainloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
-
-
-for epoch in range(1):  # loop over the dataset multiple times
+for epoch in range(3):  # loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(trainloader, start=0):
-
-        label = labels(data, masked_spots).long()
+        sequence, index = data
 
         # zero the parameter gradients
         optimizer.zero_grad()
 
-        output = pre_train(data, {0: 20, 20: 38, 38: 58}, None)
+        outputs = pre_train(sequence, {0: 20, 20: 38, 38: 58}, None)
 
         # get predicted masked tokens
-        out_pred = output[0, masked_spots[0]]
-        for n in range(2):
-            out_pred = torch.cat((out_pred, output[n, masked_spots[n]]), 0)
+        out_pred = outputs[0, masked_spots[index[0]]]
+        for n in range(1,batch_size):
+            out_pred = torch.cat((out_pred, outputs[n, masked_spots[index[n]]]), 0)
 
-        loss = criterion(out_pred, label.long())
+        # get masked token labels
+        labels_per_batch = []
+        for n in range(batch_size):
+            labels_per_batch = labels_per_batch + labels_all[index[n]]
+        labels_per_batch = torch.Tensor(labels_per_batch)
+        # dimension check
+        #print(out_pred.shape, labels_per_batch.shape)
+
+        loss = criterion(out_pred, labels_per_batch.long())
         loss.backward()
         optimizer.step()
 
-        # print statistics
+        # print loss statistics
         running_loss += loss.item()
-        if i % 10 == 9:  # print every 10 mini-batches
-            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 10))
+        if i % 3 == 2:  # print every 3 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 3))
             running_loss = 0.0
 
-#print('Finished Training')
-
-'''for epoch in range(3):
-    running_loss = 0.0
-    output = pre_train(input, {0:20, 20:38, 38:58}, None)
-    out_pred = output[0, masked_spots[0]]
-    n = len(input_seqs)
-    for i in range(1, n):
-        out_pred = torch.cat((out_pred, output[i, masked_spots[i]]), 0)
-    loss = criterion(out_pred, label)
-    loss.backward()
-    # print statistics
-    running_loss += loss.item()
-    print('[%d] loss: %.3f' % (epoch + 1, running_loss))'''
+print('Finished Training')
